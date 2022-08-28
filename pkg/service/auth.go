096849a4b0c091e2a44311a0f353f70a2de7b14e
Vlad <kwetrus@yandex.ru>
2022-08-28 17:46:25 +0300

    add parsing for header

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"crypto/sha1"
+	"errors"
 	"fmt"
 	"time"
 	restgo "todo-app"
@@ -12,8 +13,8 @@ import (
 
 const (
 	tokenTTL   = 12 * time.Hour
-	salt       = "asdk,asio[hjfp[gbhuyaf"
-	signingKey = "asdfhiasgwqrfqrhpf8ivasfag"
+	salt       = "aasdadkasiohjfp[gbhuyaf"
+	signingKey = "asdfhiasgwqrfq"
 )
 
 type tokenClaims struct {
@@ -35,9 +36,7 @@ func (s *AuthService) CreateUser(user restgo.User) (int, error) {
 }
 
 func (s *AuthService) GenerateToken(username, password string) (string, error) {
-	// get user from DB
 	user, err := s.repo.GetUser(username, generatePasswordHash(password))
-
 	if err != nil {
 		return "", err
 	}
@@ -53,8 +52,27 @@ func (s *AuthService) GenerateToken(username, password string) (string, error) {
 	return token.SignedString([]byte(signingKey))
 }
 
-func generatePasswordHash(password string) string {
+func (s *AuthService) ParseToken(accessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
 
+		return []byte(signingKey), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(*tokenClaims)
+	if !ok {
+		return 0, errors.New("token claims are not of type *tokenClaims")
+	}
+
+	return claims.UserId, nil
+}
+
+func generatePasswordHash(password string) string {
 	hash := sha1.New()
 	hash.Write([]byte(password))
 
